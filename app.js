36a5889969597d91a49a557c02f056905a3817f3
jcn13 <jcn13@uol.com.br>
2017-03-16 16:25:08 -0700

    gitignore

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function add () {
-    let item = document.getElementById('addTodoItem').value
+    let item = {name: document.getElementById('addTodoItem').value, value:0}
     addTodoItem = JSON.parse(localStorage.getItem('list'))
     if (addTodoItem === null){
         addTodoItem = []
@@ -19,17 +19,23 @@ function remove () {
     return false
 }
  
-function show () {    
+ function show () {    
     let list = JSON.parse(localStorage.getItem('list')) 
     if (list !== null){
         let html =''
         for(let i=0; i<list.length; i++) {
-            html += '<li>' + list[i] + '<button class="delete" id="' + i + '"><span>Delete</span></button></li>'
+            if(list[i].value === 1 ){
+            html += '<li class="checked" id="' + i + '">' + list[i].name + '<button class="delete" id="' + i + '"><span>Delete</span></button></li>'
         }
+        else{
+            html += '<li id="' + i + '">' + list[i].name + '<button class="delete" id="' + i + '"><span>Delete</span></button></li>' 
+        }
+    }
         document.getElementById('todoList').innerHTML = html 
         let buttons = document.getElementsByClassName('delete')
         for (let i=0; i < buttons.length; i++) {
-            buttons[i].addEventListener('click', remove)
+            buttons[i].addEventListener('click', remove)            
+
         }
     }
     else {return false}     
@@ -39,6 +45,17 @@ let lists = document.querySelector('ol')
 	lists.addEventListener('click', function(ev) {
   		if (ev.target.tagName === 'LI') {
     		ev.target.classList.toggle('checked')
+            let id = ev.target.getAttribute('id')            
+            if (ev.target.getAttribute('class') === 'checked'){
+                let list = JSON.parse(localStorage.getItem('list')) 
+                list[id].value = 1
+                localStorage.setItem('list', JSON.stringify(list))
+            }
+            else{
+                let list = JSON.parse(localStorage.getItem('list'))
+                list[id].value = 0 
+                localStorage.setItem('list', JSON.stringify(list))
+            }
   		}
 	}, false)
 
